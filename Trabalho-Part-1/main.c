df34f0c28ae67c48845d5737ab80d59b58e19bb4
thiagola92 <thiagola92@gmail.com>
2019-09-07 22:29:26 -0300

    Create test script

--- Trabalho-Part-1/main.c
+++ Trabalho-Part-1/main.c
@@ -4,16 +4,13 @@ int yywrap();
 void test_file(const char* file_path);
 
 int main(int argc, const char* argv[]) {
-	printf("Inicializando\n");
-
-	for(int i = 1; i < argc; i++) {
-		test_file(argv[i]);
+	if(argc < 2) {
+		fprintf(stderr, "Missing file path\n");
+		return 1;
 	}
-}
 
-void test_file(const char* file_path) {
-	yyin = fopen(file_path, "r");
-	printf(">> %d", yylex());
+	yyin = fopen(argv[1], "r");
+	yylex();
 }
 
 int yywrap() {
