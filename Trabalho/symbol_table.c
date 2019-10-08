75fda726aef09f11e44364aa99732abc0ffd38c8
thiagola92 <thiagola92@gmail.com>
2019-10-07 22:25:45 -0300

    Add symbol table .c and .h incomplete

---
+++ Trabalho/symbol_table.c
@@ -0,0 +1,16 @@
+// Nome: Thiago Lages de Alencar
+// Matricula: 1721629
+
+#include<symbol_table.h>
+
+void enter_scope() {
+}
+
+void leave_scope() {
+}
+
+void insert_id() {
+}
+
+Node* get_id() {
+}
---
+++ Trabalho/symbol_table.h
@@ -0,0 +1,4 @@
+// Nome: Thiago Lages de Alencar
+// Matricula: 1721629
+
+#include<node.h>
