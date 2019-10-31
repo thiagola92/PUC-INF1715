d8c1949afb626e8d00f88c9d8d6410fe0ddc3567
thiagola92 <thiagola92@gmail.com>
2019-10-31 00:17:36 -0300

    Temp backup

---
+++ Trabalho/type2.h
@@ -0,0 +1,13 @@
+// Nome: Thiago Lages de Alencar
+// Matricula: 1721629
+
+#ifndef _TYPE_
+#define _TYPE_
+
+#include"node.h"
+
+void check_condition_expression(Node* node);
+
+void type_node(Node* node);
+
+#endif
